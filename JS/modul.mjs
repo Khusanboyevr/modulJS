a04684893e15dc1a1d18520e6c91b2200a2e25ff
a <a>
2025-08-18 11:48:31 +0500

    first commit

---
+++ JS/modul.mjs
@@ -0,0 +1,22 @@
+import {
+  ShowAgeD,
+  printName,
+  AddNumberD,
+  greet,
+  fullName,
+  rectangPerimetr,
+  triangleSides,
+  squareNumber,
+  animalInfo,
+  sayHello,
+} from "./function.mjs";
+ShowAgeD(20);
+AddNumberD(5, 7);
+printName("Ali", "Vali", "Hasan", "Husan", "Olim");
+greet("Akbarjon");
+fullName("Ali", "Valiyev");
+rectangPerimetr(10, 5);
+triangleSides(3, 4, 5);
+squareNumber(6);
+animalInfo("it", 3);
+sayHello();
