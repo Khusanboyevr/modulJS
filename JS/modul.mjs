d37b8efa034a737a5443f166b60661a97aeadb24
rahmatillo <rahmatillo>
2025-08-19 14:02:14 +0500

    app js

--- JS/modul.mjs
+++ JS/modul.mjs
@@ -9,11 +9,11 @@ import {
   squareNumber,
   animalInfo,
   sayHello,
-} from "./function.mjs";
+} from "./function.js";
 ShowAgeD(20);
 AddNumberD(5, 7);
-printName("Ali", "Vali", "Hasan", "Husan", "Olim");
-greet("Akbarjon");
+printName("Ali", "Vali", "Aliya", "ALibobo", "Salom");
+greet("Ali");
 fullName("Ali", "Valiyev");
 rectangPerimetr(10, 5);
 triangleSides(3, 4, 5);
